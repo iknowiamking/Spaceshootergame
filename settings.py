cc65a681f27d89cb797dc154cbbdad257c6cd10a
Iknowiamking <balamac@mac.local>
2025-03-13 11:49:17 +0530

    change game settings

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ class Settings:
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = 60, 60, 60
-        self.bullets_allowed = 10
+        self.bullets_allowed = 5
 
         #ufo settings
         
@@ -19,14 +19,14 @@ class Settings:
         self.ship_limit = 3
 
         #level up
-        self.speedup_scale = 1.1
+        self.speedup_scale = 1.2
         self.score_scale = 1.5
         self.initialize_dynamic_settings()
 
     def initialize_dynamic_settings(self):
         """Initialize settings that change throughout the game."""
-        self.ship_speed_factor = 6
-        self.bullet_speed_factor = 10
+        self.ship_speed_factor = 3
+        self.bullet_speed_factor = 5
         self.ufo_speed_factor = 1
         # fleet_direction of 1 represents right; -1 represents left.
         self.fleet_direction = 1
